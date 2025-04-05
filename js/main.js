cc133467e18ffd984dc4b8eaea6fe2149e4778a1
Orhan <mymail@gmail.com>
2025-04-05 16:06:34 +0300

    some minor changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
+const burgerMenu = document.querySelector('.burger-menu');
 
+const openMenu = ((event)=>{
+  const burgerMenuList = document.querySelector('.burger-menu__list');
+
+  burgerMenu.classList.toggle('menu--active');
+  burgerMenuList.classList.toggle('menu--active');
+
+});
+
+burgerMenu.addEventListener('click',openMenu);
